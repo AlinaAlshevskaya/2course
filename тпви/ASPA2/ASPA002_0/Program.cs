6c4b6baf66566adf20ab03d702a7fe740361aca8
Alina Alshevskaya <alina.alshevskaya@bk.ru>
2025-09-14 23:08:07 +0300

    tpvi

---
+++ тпви/ASPA2/ASPA002_0/Program.cs
@@ -0,0 +1,22 @@
+    
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
+
+
+
+        app.UseWelcomePage("/aspnetcore");
+        app.MapGet("/aspnetcore", () => "Hi there!");
+        
+
+        app.UseDefaultFiles();
+        app.UseStaticFiles();
+
+
+        app.Run();
+    }
+}
---
+++ тпви/ASPA_006/ASPA_006_1/CelebritiesConfig.cs
@@ -0,0 +1,9 @@
+﻿
+namespace ASPA_006_1
+{
+    public class CelebritiesConfig
+    {
+        public string PhotosFolder { get; set; }
+        public string ConnectionString { get; set; }
+    }
+}
